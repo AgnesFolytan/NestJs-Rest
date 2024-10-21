f4dfd645acf300bb27011e662e0d3ada290219d5
AgnesFolytan <folytanagnes@gmail.com>
2024-10-21 14:27:41 +0200

    2024_10_21_ora vege

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,6 @@
-import { Controller, Get, Render } from '@nestjs/common';
+import { Controller, Get, Render, Param, Delete, Post, Body, NotFoundException} from '@nestjs/common';
 import { AppService } from './app.service';
+import { create } from 'domain';
 
 @Controller()
 export class AppController {
@@ -11,5 +12,44 @@ export class AppController {
     return {
       message: this.appService.getHello()
     };
+  }
+
+  #products = [
+    {
+      name: 'Bucket',
+      price: 3500,
+    },
+    {
+      name: 'Rest APi dummies',
+      price: 7850,
+    },
+    {  
+      name: 'Tablet',
+      price: 450000,
+    }
+  ];
+
+  @Get('products')
+  listProducts(){
+    return this.#products;
+  }
+
+  @Get('products/:id')
+  getProduct(@Param('id') id: string){
+    return this.#products[Number(id)];
+  }
+
+  @Delete('products/:id')
+  deleteProduct(@Param('id') id: string){
+    const idNum = Number(id)
+    if (idNum >= this.#products.length || idNum < 0) {
+      throw new NotFoundException("No product with this ID")
+    }
+    this.#products.splice(Number(id), 1);
+  }
+
+  @Post('products')
+  newProducts(@Body() createProductDto: createProductDto) {
+    this.#products.push(createProductDto);
   }
 }
---
+++ src/createproduct.dto.ts
@@ -0,0 +1,14 @@
+import {IsDefined, IsNumber, IsString, Min } from "class-validator"
+
+export class createProductDto {
+    @IsDefined({
+        message: "The product name is required!"
+    })
+    @IsString()
+    name: string;
+
+    @IsDefined()
+    @IsNumber()
+    @Min(1, {message: "The price must be a positive number."})
+    price: number;
+}
